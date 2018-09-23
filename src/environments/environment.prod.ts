c0396404d081ea5b936f0a3cdd74011971b171f0
Ritu Raghani <ritu.raghani@auxesisgroup.com>
2018-09-23 21:37:31 +0530

    commit changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBod8iQ--Dx7y1vui3-FDf07jvI7vd1410",
+    authDomain: "clients-ef364.firebaseapp.com",
+    databaseURL: "https://clients-ef364.firebaseio.com",
+    projectId: "clients-ef364",
+    storageBucket: "clients-ef364.appspot.com",
+    messagingSenderId: "464583651594"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBod8iQ--Dx7y1vui3-FDf07jvI7vd1410",
+    authDomain: "clients-ef364.firebaseapp.com",
+    databaseURL: "https://clients-ef364.firebaseio.com",
+    projectId: "clients-ef364",
+    storageBucket: "clients-ef364.appspot.com",
+    messagingSenderId: "464583651594"
+  }
 };
 
 /*
